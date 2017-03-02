3823729faf17d9445085b402dde6d9ffd18c825a
JAZZID <JAZZID@JAZZID>
2017-03-02 13:46:46 +0200

    Задание 3

---
+++ AddressBook_WebTest/AddressBook_WebTest/AccountData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressBookTests
+{
+    class AccountData
+    {
+        private string username;
+        private string password;
+
+        public AccountData(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public string UserName
+        {
+            get
+            {
+                return username;
+            }
+
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                username = value;
+            }
+        }
+
+
+    }
+}
--- AddressBook_WebTest/AddressBook_WebTest/GroupCreationTests.cs
+++ AddressBook_WebTest/AddressBook_WebTest/GroupCreationTests.cs
@@ -20,9 +20,7 @@ namespace WebAddressBookTests
         [SetUp]
         public void SetupTest()
         {
-            //для версии firefox 51.0.1 (64-бит) не отработал тест
-            //пришлось ставить ESR версию
-            driver = new FirefoxDriver(new FirefoxBinary("C:\\Program Files\\Mozilla Firefox ESR\\firefox.exe"), new FirefoxProfile() );
+            driver = new FirefoxDriver(new FirefoxBinary("C:\\Program Files\\Mozilla Firefox ESR\\firefox.exe"), new FirefoxProfile());
             baseURL = "http://localhost/";
             verificationErrors = new StringBuilder();
         }
@@ -44,24 +42,87 @@ namespace WebAddressBookTests
         [Test]
         public void GroupCreationTest()
         {
-            driver.Navigate().GoToUrl(baseURL + "addressbook/");
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
-            driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.Name("new")).Click();
-            driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("Group1");
-            driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("GroupHeader");
-            driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("GroupFooter");
-            driver.FindElement(By.Name("submit")).Click();
-            driver.FindElement(By.LinkText("group page")).Click();
+            OpenHomePage();
+            //Инициализация пользователя
+            //AccountData login = new AccountData();
+            //login.UserName = "admin";
+            //login.UserName = "secret";
+            
+            Login(new AccountData("admin", "secret"));
+
+            GoToGroupsPage();
+
+            //Создание группы
+            InitGroupCreation();
+            GroupData group = new GroupData();
+                group.Name = "Group1";
+                group.Header = "GroupHeader";
+                group.Footer = "GroupFooter";
+
+            FillGroupForm(group);
+            SubmitGroupCreation();
+
+            ReturnToGroupsPage();
+            Logout();
+        }
+
+        private void Logout()
+        {
+            // Logout
             driver.FindElement(By.LinkText("Logout")).Click();
         }
+
+        private void ReturnToGroupsPage()
+        {
+            // Return to groups page
+            driver.FindElement(By.LinkText("group page")).Click();
+        }
+
+        private void SubmitGroupCreation()
+        {
+            // Submit group creation
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillGroupForm(GroupData group)
+        {
+            // Fill group form
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
+        }
+
+        private void InitGroupCreation()
+        {
+            // Init new group creation
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void GoToGroupsPage()
+        {
+            // Go to groups page
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void Login(AccountData account)
+        {
+            // Login as Admin
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.UserName);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            // Open home page
+            driver.Navigate().GoToUrl(baseURL + "addressbook/");
+        }
+
         private bool IsElementPresent(By by)
         {
             try
