82a6d34265890c30bf2f3d280f1dfca629c9b8cc
JAZZID <JAZZID@JAZZID>
2017-03-03 12:45:55 +0200

    Задание #4

---
+++ AddressBook_WebTest/AddressBook_WebTest/ContactCreationTests.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressBookTests
+{
+    [TestFixture]
+    public class ContactCreationTests
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver(new FirefoxBinary("C:\\Program Files\\Mozilla Firefox ESR\\firefox.exe"), new FirefoxProfile());
+            baseURL = "http://localhost/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void ContactCreationTest()
+        {
+            driver.Navigate().GoToUrl(baseURL + "addressbook/");
+
+            //Инициализация пользователя
+            Login(new AccountData("admin", "secret"));
+
+            ContactData contact = new ContactData();
+                contact.LastName = "Иванов";
+                contact.FirstName = "Иван";
+                contact.MiddleName = "Иванович";
+
+            PersonalInfo(contact);
+            WorkInfo(contact);
+            BasicContactInfo(contact);
+            AdditionalContactInfo(contact);
+            SubmitContact();
+            GoToHomePage();
+            Logout();
+        }
+
+        private void PersonalInfo(ContactData contact)
+        {
+            //Персональные данные
+            driver.FindElement(By.LinkText("add new")).Click();
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(contact.FirstName);
+            driver.FindElement(By.Name("middlename")).Clear();
+            driver.FindElement(By.Name("middlename")).SendKeys(contact.MiddleName);
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(contact.LastName);
+            driver.FindElement(By.Name("nickname")).Clear();
+            driver.FindElement(By.Name("nickname")).SendKeys(contact.NickName);
+            //Дата рождения
+            new SelectElement(driver.FindElement(By.Name("bday"))).SelectByText(contact.BDay);
+            new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText(contact.BMonth);
+            driver.FindElement(By.Name("byear")).Clear();
+            driver.FindElement(By.Name("byear")).SendKeys(contact.BYear);
+            //Годовщина
+            new SelectElement(driver.FindElement(By.Name("aday"))).SelectByText(contact.ADay);
+            new SelectElement(driver.FindElement(By.Name("amonth"))).SelectByText(contact.AMonth);
+            driver.FindElement(By.Name("ayear")).Clear();
+            driver.FindElement(By.Name("ayear")).SendKeys(contact.AYear);
+        }
+
+        private void WorkInfo(ContactData contact)
+        {
+            //Сведения о работе
+            driver.FindElement(By.Name("title")).Clear();
+            driver.FindElement(By.Name("title")).SendKeys(contact.Title);
+            driver.FindElement(By.Name("company")).Clear();
+            driver.FindElement(By.Name("company")).SendKeys(contact.Company);                                                           
+            driver.FindElement(By.Name("address")).Clear();
+            driver.FindElement(By.Name("address")).SendKeys(contact.Address);
+        }
+
+        private void BasicContactInfo(ContactData contact)
+        {
+            //Контактные данные
+            driver.FindElement(By.Name("home")).Clear();
+            driver.FindElement(By.Name("home")).SendKeys(contact.Home);
+            driver.FindElement(By.Name("mobile")).Clear();
+            driver.FindElement(By.Name("mobile")).SendKeys(contact.Mobile);
+            driver.FindElement(By.Name("work")).Clear();
+            driver.FindElement(By.Name("work")).SendKeys(contact.Work);
+            driver.FindElement(By.Name("fax")).Clear();
+            driver.FindElement(By.Name("fax")).SendKeys(contact.Fax);
+            driver.FindElement(By.Name("email")).Clear();
+            driver.FindElement(By.Name("email")).SendKeys(contact.Email);
+            driver.FindElement(By.Name("email2")).Clear();
+            driver.FindElement(By.Name("email2")).SendKeys(contact.Email2);
+            driver.FindElement(By.Name("email3")).Clear();
+            driver.FindElement(By.Name("email3")).SendKeys(contact.Email3);
+            driver.FindElement(By.Name("homepage")).Clear();
+            driver.FindElement(By.Name("homepage")).SendKeys(contact.Homepage);
+        }
+
+        private void AdditionalContactInfo(ContactData contact)
+        {
+            //Дополнительные данные
+            driver.FindElement(By.Name("address2")).Clear();
+            driver.FindElement(By.Name("address2")).SendKeys(contact.Address2);
+            driver.FindElement(By.Name("phone2")).Clear();
+            driver.FindElement(By.Name("phone2")).SendKeys(contact.Phone2);
+            driver.FindElement(By.Name("notes")).Clear();
+            driver.FindElement(By.Name("notes")).SendKeys(contact.Notes);
+        }
+
+        private void SubmitContact()
+        {
+            //Сохранение контакта
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void GoToHomePage()
+        {
+            //Go to home page
+            driver.FindElement(By.LinkText("home")).Click();
+        }
+
+        private void Logout()
+        {
+            //Logout
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void Login(AccountData account)
+        {
+            // Login as Admin
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.UserName);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
---
+++ AddressBook_WebTest/AddressBook_WebTest/ContactData.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressBookTests
+{
+    class ContactData
+    {
+        //Персональные данные
+        private string firstname = ""; 
+        private string middlename = "";
+        private string lastname = "";
+        private string nickname = "0";
+
+        //Дата рождения
+        private string bday = "1";
+        private string bmonth = "January";
+        private string byear = "1899";
+        //Годовщина
+        private string aday = "1";
+        private string amonth = "January";
+        private string ayear = "1899";
+
+        //Сведения о работе
+        private string title = "0";
+        private string company = "0";
+        private string address = "0";
+
+        //Контактные данные
+        private string home = "0";
+        private string mobile = "0";
+        private string work = "0";
+        private string fax = "0";
+        private string email = "0";
+        private string email2 = "0";
+        private string email3 = "0";
+        private string homepage = "0";
+
+        //Дополнительные данные
+        private string address2 = "0";
+        private string phone2 = "0";
+        private string notes = "0";
+
+
+        public string FirstName
+        {
+            get
+            {
+                return firstname;
+            }
+
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string MiddleName
+        {
+            get
+            {
+                return middlename;
+            }
+
+            set
+            {
+                middlename = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastname;
+            }
+
+            set
+            {
+                lastname = value;
+            }
+        }
+
+        public string NickName
+        {
+            get
+            {
+                return nickname;
+            }
+
+            set
+            {
+                nickname = value;
+            }
+        }
+
+
+        public string BDay
+        {
+            get
+            {
+                return bday;
+            }
+
+            set
+            {
+                bday = value;
+            }
+        }
+
+        public string BMonth
+        {
+            get
+            {
+                return bmonth;
+            }
+
+            set
+            {
+                bmonth = value;
+            }
+        }
+
+        public string BYear
+        {
+            get
+            {
+                return byear;
+            }
+
+            set
+            {
+                byear = value;
+            }
+        }
+
+        public string ADay
+        {
+            get
+            {
+                return aday;
+            }
+
+            set
+            {
+                aday = value;
+            }
+        }
+
+        public string AMonth
+        {
+            get
+            {
+                return amonth;
+            }
+
+            set
+            {
+                amonth = value;
+            }
+        }
+
+        public string AYear
+        {
+            get
+            {
+                return ayear;
+            }
+
+            set
+            {
+                ayear = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Company
+        {
+            get
+            {
+                return company;
+            }
+
+            set
+            {
+                company = value;
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+
+            set
+            {
+                address = value;
+            }
+        }
+
+        public string Home
+        {
+            get
+            {
+                return home;
+            }
+
+            set
+            {
+                home = value;
+            }
+        }
+
+        public string Mobile
+        {
+            get
+            {
+                return mobile;
+            }
+
+            set
+            {
+                mobile = value;
+            }
+        }
+
+        public string Work
+        {
+            get
+            {
+                return work;
+            }
+
+            set
+            {
+                work = value;
+            }
+        }
+
+        public string Fax
+        {
+            get
+            {
+                return fax;
+            }
+
+            set
+            {
+                fax = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string Email2
+        {
+            get
+            {
+                return email2;
+            }
+
+            set
+            {
+                email2 = value;
+            }
+        }
+
+        public string Email3
+        {
+            get
+            {
+                return email3;
+            }
+
+            set
+            {
+                email3 = value;
+            }
+        }
+
+        public string Homepage
+        {
+            get
+            {
+                return homepage;
+            }
+
+            set
+            {
+                homepage = value;
+            }
+        }
+
+        public string Address2
+        {
+            get
+            {
+                return address2;
+            }
+
+            set
+            {
+                address2 = value;
+            }
+        }
+
+        public string Phone2
+        {
+            get
+            {
+                return phone2;
+            }
+
+            set
+            {
+                phone2 = value;
+            }
+        }
+
+        public string Notes
+        {
+            get
+            {
+                return notes;
+            }
+
+            set
+            {
+                notes = value;
+            }
+        }
+
+
+    }
+}
